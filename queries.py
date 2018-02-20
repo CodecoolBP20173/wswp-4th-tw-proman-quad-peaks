d6947380d4884f5920757762b55abc7a4df7313d
AndrasNyiri <nyiriandriss@gmail.com>
2018-02-20 09:37:25 +0100

    save board data to database implemented

--- queries.py
+++ queries.py
@@ -2,7 +2,6 @@ import data_manager
 
 
 def get_data(group_id):
-
     boards = data_manager.execute_select("""
                                         SELECT id,title,is_active FROM boards WHERE group_id=%(group_id)s
                                         """, {'group_id': group_id})
@@ -22,4 +21,15 @@ def get_data(group_id):
 
 
 def save_data(group_id, cards, boards):
-    pass
+    for board in boards:
+        data_manager.execute_dml_statement("""UPDATE boards SET title=%(title)s,is_active=%(is_active)s
+                                        WHERE id=%(board_id)s AND group_id=%(group_id)s""",
+                                           {'title': board['title'], 'is_active': board['is_active'],
+                                            'board_id': board['id'], 'group_id': group_id})
+
+    for card in cards:
+        data_manager.execute_dml_statement("""UPDATE cards SET title=%(title)s,board_id=%(board_id)s,status_id=%(status_id)s,"order"=%(order)s
+                                              WHERE id=%(card_id)s
+                                            """, {'title': card['title'], 'board_id': card['board_id'],
+                                                  'status_id': card['status_id'], 'order': card['order'],
+                                                  'card_id': card['id']})
