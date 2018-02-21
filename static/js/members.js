70ed678c24a3bb67c47508f6cbd44d572f10ee66
Adam Grob <ad4mgr0b@gmail.com>
2018-02-21 15:30:22 +0100

    Wokring search capability of the members html page. Minor css styling

--- static/js/members.js
+++ static/js/members.js
@@ -1,26 +1,66 @@
 let kick_buttons = document.getElementsByClassName('button-kick-member');
 
+let search_pattern_holder = document.getElementById('search-account-input');
+
+
+function searchListener() {
+    search_pattern_holder.addEventListener('input', function () {
+        if (search_pattern_holder.value.length > 0) {
+            $.getJSON('search/' + search_pattern_holder.value, function (response) {
+                if (response && response.length > 0) {
+                    results = response;
+                    document.getElementById('table_holder_for_search').innerHTML = '<table id="search_results_table">'
+                        + '</table>';
+                    searchResultsTableMaker(results)
+                } else {
+                    document.getElementById('table_holder_for_search').innerHTML = '<h3>No Results</h3>';
+                }
+            });
+        }
+
+    });
+
+}
+
+
 function kickButtonListener() {
     for (i = 0; i < kick_buttons.length; i++) {
         kick_buttons[i].addEventListener('click', function () {
-            $.ajax({
-                dataType: "text",
-                url: 'delete_user',
-                data: {
-                    'id': $(this).id
-                },
-                cache: false,
-                type: "POST",
-                success: function (response) {
-                    console.log(response);
-                },
-                error: function (xhr) {
-                    alert('something went wrong');
-                }
-            });
-            $(this).parent().parent().remove();
-        }
+                $.ajax({
+                    dataType: "text",
+                    url: 'delete_user',
+                    data: {
+                        'id': $(this).id
+                    },
+                    cache: false,
+                    type: "POST",
+                    success: function (response) {
+                        console.log(response);
+                    },
+                    error: function (xhr) {
+                        alert('something went wrong');
+                    }
+                });
+                $(this).parent().parent().remove();
+            }
         )
     }
 }
+
+function searchResultsTableMaker(listOfDicts) {
+    let contentClean = '';
+    for (i = 0; i < listOfDicts.length; i++) {
+        console.log(listOfDicts[i].username);
+        contentClean += `<tr><td>`
+            + listOfDicts[i].username
+            + `</td><td><button class="button-add-member">Fel vagy véve</button></td></tr>`
+    }
+    document.getElementById('search_results_table').innerHTML = contentClean;
+}
+
 kickButtonListener();
+
+
+searchListener();
+
+addMemberListener();
